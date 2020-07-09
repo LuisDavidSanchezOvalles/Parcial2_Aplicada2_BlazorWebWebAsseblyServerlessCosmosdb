1e8bb0eaa8683d170024e6225525c7e615e11c65
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-07-08 23:24:10 -0400

    Add project files.

---
+++ LDSOFAQFunctionApp/CovidFAQ.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs;
+using LDSOFAQFunctionApp.Models;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Threading.Tasks;
+
+namespace LDSOFAQFunctionApp
+{
+    class CovidFAQ
+    {
+        [FunctionName("covidFAQ")]
+        public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        [CosmosDB(
+            //Aqui va como llamaste la base de datos en COSMOS DB en mi caso fue LDSOFAQDB y la collection como LDSOFAQContainer
+            databaseName:"LDSOFAQDB",
+            collectionName:"LDSOFAQContainer",
+            ConnectionStringSetting = "DBConnectionString"
+            )] IEnumerable<FAQ> questionSet,
+        ILogger log)
+        {
+            log.LogInformation("Data fetched from FAQContainer");
+            return new OkObjectResult(questionSet);
+        }
+    }
+}
